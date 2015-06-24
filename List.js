026f098b7d7f8b0217ae589e87e30f0c9867b230
unknown <593152@01HW346354.India.TCS.com>
2015-06-24 16:41:21 +0530

    Adding complete List.js file for the exercise from Chapter 4 in Eloquent Javascript book

--- List.js
+++ List.js
@@ -5,6 +5,32 @@ var arrayToList = function(InputArray){
   return OutputList;
   }
 
+function listToArray(OutputList) {
+  var array = [];
+  for (var node = OutputList; node.rest != null; node = node.rest)
+    array.push(node.value);
+  return array;
+}
+
+function prepend(value, OutputList) {
+  return {value: value, rest: OutputList};
+}
+
+function nth(OutputList, n) {
+  if (!OutputList)
+    return undefined;
+  else if (n == 0)
+    return OutputList.value;
+  else
+    return nth(OutputList.rest, n - 1);
+}
+
 
 console.log(arrayToList([10, 20]));
-//  {value: 10, rest: {value: 20, rest: null}}
+// ? {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// ? [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// ? {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// ? 20
